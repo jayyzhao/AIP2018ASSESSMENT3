bc4c1253fc01d4a8524e106c348a1649d4414de7
jayyzhao <jzhao93@gmail.com>
2018-08-20 18:40:19 +1000

    MSSQL databse connection

--- server.js
+++ server.js
@@ -1,14 +1,40 @@
 const express = require('express');
-
 const app = express();
+
+var sql = require("mssql");
+
+var config = {
+    user: 'adm',
+    password: 'Password1234',
+    server: 'book-a-table.database.windows.net', 
+    database: 'book-a-table', 
+
+    options: {
+        encrypt: true // Use this if you're on Windows Azure
+    }
+
+};
+
+sql.connect(config, function (err) {
+    
+    if (err) console.log(err);
+});
+
+
 
 app.get('/api/test', (req, res) => {
 
-    const customers = [
-        {id: 1, firstName: 'John', lastName: 'Doe'}
-    ];
-
-    res.json(customers);
+     // create Request object
+     var request = new sql.Request();
+       
+     // query to the database and get the records
+     request.query('select * from Users', function (err, recordset) {
+         
+         if (err) console.log(err)
+         // send records as a response
+         res.send(recordset);
+         
+     });
 
 });
 
