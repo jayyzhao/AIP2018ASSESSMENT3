f442abab9ae6a8efe483f1dc11805d62d2b439dd
sunwenyang <573534939@qq.com>
2018-10-01 21:44:51 +1000

    add a user button on nav bar

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 For Development  - run 'npm run dev' to start both client and server.
 Do 'npm install --save vanilla-back-to-top' please to get the button
 
+npm i --save @fortawesome/fontawesome-svg-core     
+
+ npm i --save @fortawesome/free-solid-svg-icons    
+  
+ npm i --save @fortawesome/react-fontawesome     
+
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import Authentication from './Authentication';
 import decode from 'jwt-decode';
+import "./css/Navigation.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import {
+  Collapse,
+  NavbarToggler,
+  NavbarBrand,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
 
 const NavItem = props => {
   const pageURI = window.location.pathname+window.location.search
@@ -54,7 +68,7 @@ class Navigation extends React.Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/">Book-A-Table</a>
+        <a className="navbar-brand" href="/" style={{fontSize:30}}>Book-A-Table</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -65,17 +79,32 @@ class Navigation extends React.Component {
             <NavItem path="/Book" name="Book" />            
           </ul>
           {this.state.user.USERS_ID > 0 ? (
-            <ul className="navbar-nav my-2 my-lg-0">
-              <span>Welcome {this.state.user.USERS_FIRST_NAME} {this.state.user.USERS_LAST_NAME}</span>
+            <ul className="navbar-nav my-1 my-lg-0" style={{color:'white',fontStyle:'italic'}}>
+              <span className="userInfo">Welcome {this.state.user.USERS_FIRST_NAME} {this.state.user.USERS_LAST_NAME}</span> 
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                <FontAwesomeIcon className="userIcon" icon={faUserCircle} size="lg"></FontAwesomeIcon>
+                </DropdownToggle>
+                <DropdownMenu right className="userIconDropDown" style={{fontSize:'15px'}}>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Logout
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
             </ul>
           ): 
-            <ul className="navbar-nav my-2 my-lg-0">
+            <ul className="navbar-nav my-2 my-lg-0" >
               <NavItem path="/signup" name="Sign Up"/>
               <NavItem path="/login" name="Login"/>
             </ul>
           }
-          
-          
         </div>
       </nav>
     )
