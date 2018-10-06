38a4525ad784b927b1a1482327fcc693fb7b3785
jayyzhao <jzhao93@gmail.com>
2018-10-06 19:09:06 +1000

    Update

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -53,7 +53,6 @@ class Navigation extends React.Component {
   }
 
   render() {
-<<<<<<< HEAD
     const loggedIn = localStorage.getItem('id_token');
     if(loggedIn){
       const decoded = decode(localStorage.getItem('id_token'));
@@ -105,47 +104,6 @@ class Navigation extends React.Component {
             <ul className="navbar-nav mr-auto">
               <NavItem path="/" name="Home" />
               <NavItem path="/Book" name="Book" />            
-=======
-    return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="/" style={{fontSize:30}}>Book-A-Table</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <NavItem path="/" name="Home" />
-            <NavItem path="/Book" name="Book" />  
-			<NavItem path="/MyBookings" name="My Bookings" />  
-			<NavItem path="/MyRestaurants" name="My Restaurants" />  
-          </ul>
-          {this.state.user.USERS_ID > 0 ? (
-            <ul className="navbar-nav my-1 my-lg-0" style={{color:'white',fontStyle:'italic'}}>
-              <span className="userInfo">Welcome {this.state.user.USERS_FIRST_NAME} {this.state.user.USERS_LAST_NAME}</span> 
-              <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                <FontAwesomeIcon className="userIcon" icon={faUserCircle} size="lg"></FontAwesomeIcon>
-                </DropdownToggle>
-                <DropdownMenu right className="userIconDropDown" style={{fontSize:'15px'}}>
-                  <DropdownItem>
-                    Option 1
-                  </DropdownItem>
-                  <DropdownItem>
-                    Option 2
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem>
-                    Logout
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </ul>
-          ): 
-            <ul className="navbar-nav my-2 my-lg-0" >
-              <NavItem path="/signup" name="Sign Up"/>
-              <NavItem path="/login" name="Login"/>
->>>>>>> 4af9bd14789c4081b40ba5d7b56a25240a232f71
             </ul>
             {this.state.user.USERS_ID > 0 ? (
               <ul className="navbar-nav my-1 my-lg-0" style={{color:'white',fontStyle:'italic'}}>
